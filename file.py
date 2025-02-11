2862fbc6f895a863fd1eb7e5c74b1316fc33d224
Haroun-Alameddine <alameddineharoun@gmail.com>
2025-02-11 08:53:38 +0200

    added some line

--- file.py
+++ file.py
@@ -1,3 +1,17 @@
 print("hello moutaz")
 
 print("hello haroun")
+
+print("good morning moutaz and mostafa")
+
+name = input("enter your name")
+lastname = input("enter your last name")
+greetings = name + lastname
+age = input("enter your age")
+
+print("good morning moutaz and mostafa")
+
+name = input("enter your name")
+lastname = input("enter your last name")
+greetings = name + lastname
+age = input("enter your age")
