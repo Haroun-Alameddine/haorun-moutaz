726b906077af7f296bd9048ac88d5070f9ad3d07
MostafaAlkini <mostafaalkini@gmail.com>
2025-02-11 09:13:52 +0200

    Edited sentences

--- file.py
+++ file.py
@@ -4,14 +4,20 @@ print("hello haroun")
 
 print("good morning moutaz and mostafa")
 
-name = input("enter your name")
-lastname = input("enter your last name")
+name = input("enter your name:")
+lastname = input("enter your last name:")
 greetings = name + lastname
 age = input("enter your age")
 
 print("good morning moutaz and mostafa")
 
-name = input("enter your name")
-lastname = input("enter your last name")
+name = input("enter your name:")
+lastname = input("enter your last name:")
 greetings = name + lastname
 age = input("enter your age")
+
+Mname=input("Enter name again")
+Mlastname=input("Enter Your last name")
+
+print("Hello Mostafa")
+print("My name is "+ name +"my last name is "+lastname+str(age))
