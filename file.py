b95f4d48aae2dc3a1970e7d43c66f6a041a9b0bc
Haroun-Alameddine <alameddineharoun@gmail.com>
2025-02-11 10:06:05 +0200

    added new

--- file.py
+++ file.py
@@ -21,3 +21,16 @@ Mlastname=input("Enter Your last name")
 
 print("Hello Mostafa")
 print("My name is "+ name +"my last name is "+lastname+str(age))
+
+ students = ["Alice", "Bob", "Charlie", "David"]
+num_students = len(students)
+print(f"Number of students: {num_students}")
+num_students = int(input("Enter the number of students: "))
+print(f"Total students: {num_students}")
+students = {
+    "101": "Alice",
+    "102": "Bob",
+    "103": "Charlie",
+    "104": "David"
+}
+print(f"Number of students: {len(students)}")
