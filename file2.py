80eaecb2323f79074d4bfeb7a6b644309874e4ff
MostafaAlkini <mostafaalkini@gmail.com>
2025-02-11 09:46:00 +0200

    Checked Number

--- file2.py
+++ file2.py
@@ -1,1 +1,7 @@
-print("please fill free to update this file")
+print("Check Number if Even or odd")
+
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("{0} is Even".format(num))
+else:
+   print("{0} is Odd".format(num))
