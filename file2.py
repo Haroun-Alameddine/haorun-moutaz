166c8468e09de489ec0be8ee2ed5780eab9204d4
MostafaAlkini <mostafaalkini@gmail.com>
2025-02-11 09:52:22 +0200

    Added Calendar

--- file2.py
+++ file2.py
@@ -5,3 +5,19 @@ if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
    print("{0} is Odd".format(num))
+
+
+# Program to display calendar of the given month and year
+
+# importing calendar module
+import calendar
+
+yy = int(input("Enter a Year: "))  # year
+mm = int(input("Enter a Month: "))    # month
+
+# To take month and year input from the user
+# yy = int(input("Enter year: "))
+# mm = int(input("Enter month: "))
+
+# display the calendar
+print(calendar.month(yy, mm))
